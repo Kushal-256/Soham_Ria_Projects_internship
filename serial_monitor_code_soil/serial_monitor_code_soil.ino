841ecd1f713473a3a6bdb28be78e3b6d1c773b7b
kushal manikanta <62746829+Kushal-256@users.noreply.github.com>
2022-10-19 18:42:11 +0530

    Add files via upload

--- serial_monitor_code_soil/serial_monitor_code_soil.ino
+++ serial_monitor_code_soil/serial_monitor_code_soil.ino
@@ -1,100 +1,83 @@
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define relayInput 4
 
-int condition = 0 ; 
+int condition = 0 ;
+void printText(String msg1 , String msg2) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(msg1);
+  lcd.setCursor(0, 1);
+  lcd.print(msg2);
+  delay(100);
+}
 void setup() {
   Serial.begin(9600);
   lcd.begin();
   lcd.backlight();
   lcd.clear();
   pinMode(relayInput, OUTPUT);
-  digitalWrite(relayInput, HIGH);
+  digitalWrite(relayInput, LOW);
+  delay(500);
+  printText("IRRIGATION", "SYSTEM IS ON ");
   delay(1000);
-  lcd.setCursor(0, 0);
-  lcd.print("IRRIGATION");
-  lcd.setCursor(0, 1);
-  lcd.print("SYSTEM IS ON ");
-  lcd.print("");
- delay(2000);
- pinMode(2,OUTPUT);
-  lcd.clear();
+  pinMode(2, OUTPUT);
 }
- 
+
 void loop() {
-  int value = analogRead(A0);
+  int value = analogRead(36);
   Serial.println(value);
-   if (value > 2000 && value < 4000 ) {
+  if (value > 2000 && value < 4000 ) {
     Serial.print("Moisture : LOW ");
     Serial.println("Water Pump is ON ");
+    digitalWrite(relayInput, HIGH);
+    delay(100);
+  }
+  else {
     digitalWrite(relayInput, LOW);
-    delay(50);
-  }
-  else{
-     digitalWrite(relayInput, HIGH);
-         delay(50);
+    delay(100);
   }
 
-
-  
   if (value < 1075) {
-     Serial.print("Moisture : HIGH");
-     Serial.print("Water Pump is OFF");
-    if (condition != 1){
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Pump is OFF");
-    delay(100);
-    lcd.setCursor(1, 1);
-    lcd.print("Moisture : HIGH");
-    delay(100);
-//    digitalWrite(relayInput, HIGH);
-    digitalWrite(2,LOW);
-      delay(100);
+    Serial.print("Moisture : HIGH");
+    Serial.print("Water Pump is OFF");
+    if (condition != 1) {
+      printText("Pump is OFF", "Moisture : HIGH");
+      digitalWrite(2, LOW);
       condition = 1;
+      delay(100);
     }
   } else if (value > 1075 && value < 2000) {
-     Serial.print("Water Pump is OFF");
-      Serial.print("Moisture : MID ");
-     if (condition != 2 ){
-     lcd.clear();
-      lcd.setCursor(0, 0);
-    lcd.print("Moisture : MID");
-    delay(100);
-    lcd.setCursor(1, 1);
-    lcd.print("Pump is OFF");
-    delay(100);
-//   digitalWrite(relayInput, HIGH);
-    digitalWrite(2,LOW);
-     delay(100);
-     condition = 2;
-      }
+    Serial.print("Water Pump is OFF");
+    Serial.print("Moisture : MID ");
+    if (condition != 2 ) {
+      printText("Pump is OFF", "Moisture : MID");
+      digitalWrite(2, LOW);
+      delay(100);
+      condition = 2;
+      delay(100);
+    }
   } else if (value > 2000 && value < 4000 ) {
     Serial.print("Moisture : LOW ");
     Serial.println("Water Pump is ON ");
-//    digitalWrite(relayInput, LOW);
-    if (condition != 3){
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Pump is ON ");
-    delay(200);
-    digitalWrite(2,HIGH);
-    lcd.setCursor(1, 1);
-    lcd.print("Moisture :LOW");
-    delay(200);
-    condition = 3;
+    //    digitalWrite(relayInput, LOW);
+    if (condition != 3) {
+      printText("Pump is ON", "Moisture :   LOW");
+      digitalWrite(2, HIGH);
+      condition = 3;
+      delay(100);
     }
   }
-  
-  else if(value > 4000){
-    if (condition != 4){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    digitalWrite(2,LOW);
-    
-    lcd.print("Sensor is in Air");
-    delay(500);
-    condition = 4; 
+
+  else if (value > 4000) {
+    if (condition != 4) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      digitalWrite(2, LOW);
+      printText("Sensor is in Air","");
+      condition = 4;
+      delay(100);
     }
   }
 }
